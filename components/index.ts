c86b0837cd440c2f5af98cd3d86bb88192ef9d6e
kishan-aghera <55836816+kishan-aghera@users.noreply.github.com>
2023-07-31 14:32:26 +0530

    Initial Commit.

---
+++ components/index.ts
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+export {
+  Hero,
+  Navbar,
+  Footer,
+  CustomFilter,
+  SearchBar,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+};
---
+++ env.d.ts
@@ -0,0 +1,7 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    RAPID_API_KEY: string;
+    RAPID_API_HOST: string;
+    IMAGIN_STUDIO_API_KEY: string;
+  }
+}
